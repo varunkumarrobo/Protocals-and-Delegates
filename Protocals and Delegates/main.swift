1b2c096ddc3fad9b3f669407ee2d6c23957c4a5c
varunkumarrobo <114062044+varunkumarrobo@users.noreply.github.com>
2023-04-19 18:10:13 +0530

    first commit

--- Protocals and Delegates/main.swift
+++ Protocals and Delegates/main.swift
@@ -1,11 +1,69 @@
-//
-//  main.swift
-//  Protocals and Delegates
-//
-//  Created by Varun Kumar on 17/04/23.
-//
+protocol AdvancedLifeSupport {
+    func performCPR()
+}
 
-import Foundation
+class EmergencyCallHandler {
+    var delegate :  AdvancedLifeSupport?
+    
+    
+    func assessSituation()  {
+        print("Can you tell me what happened?")
+    }
+    
+    func medicalEmergency()  {
+        delegate?.performCPR()
+    }
+    
+}
 
-print("Hello, World!")
+struct Paramedic : AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("The Paramedic does chest compressions, 30 per second.")
+    }
+     
+}
+
+
+class Doctor: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The Doctor does chest compressions, 30 per second.")
+    }
+    
+    func useStethescope()  {
+        print("Listening for heart sounds")
+    }
+}
+
+class Surgeon: Doctor {
+    
+    override func performCPR() {
+        super.performCPR()
+        print("Sings Staying alive by the BeeGeess")
+    }
+    
+    func useElectricDrill()  {
+        print("Whirrrr....")
+    }
+}
+
+let sai = EmergencyCallHandler()
+let raj = Paramedic(handler: sai)
+let me = Surgeon(handler: sai)
+ 
+
+sai.assessSituation()
+sai.medicalEmergency()
+
+
+
+
 
